bbc8d2b657b1affc4807a25fd8fb65533e3e4908
Fez <FernandoLpz0911.business@Gmail.com>
2025-08-13 10:02:52 -0500

    making and styling the home container, also clearing some useless code

--- PersonalWebpage/src/pages/Home.jsx
+++ PersonalWebpage/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './cssfiles/Home.css'
 function Home() {
   return (
     <>
-      <div>
+      <div className='home-container'>
         <h1>Fernando Lopez</h1>
         <p>Description of who i am</p>  
       </div>
