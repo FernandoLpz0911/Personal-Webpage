8c9fb3e9c890560c192b1b06165e218cb0cd4dc6
Fernando <FernandoLpz0911.business@gmail.com>
2025-08-14 12:15:19 -0500

    Architecting the structure of the page

--- PersonalWebpage/src/pages/About.jsx
+++ PersonalWebpage/src/pages/About.jsx
@@ -6,17 +6,17 @@ function About() {
     <>
       <div className='About-Page-Container'>
         <div className='Past'>
-
+          <p> Information about the past</p>
         </div>
 
         <div className='Present'>
-
+          <p> Information about the present</p>
         </div>
 
         <div className='Future'>
-
+          <p> Information about the future</p>
         </div>
-        
+
       </div>
     </>
   )
