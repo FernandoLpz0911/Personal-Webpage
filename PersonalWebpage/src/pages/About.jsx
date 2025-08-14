8d79fbdac571d8a3956c846ef32aa631ec0f0d02
Fernando <FernandoLpz0911.business@gmail.com>
2025-08-14 12:19:36 -0500

    A bit of my past

--- PersonalWebpage/src/pages/About.jsx
+++ PersonalWebpage/src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
     <>
       <div className='About-Page-Container'>
         <div className='Past'>
-          <p> Information about the past</p>
+          <p> I have 5 siblings, 3 brothers, and 2 sisters. I always used to mess with computers as a kid because I used to like install malware from the mods and my brother who is also in the field always used to have to fix it, needless to say, eventually he got tired of having to fix my messes and told me "You broke it, you fix it" and from there I learned how to use file directories and how to be more digitally safe.</p>
         </div>
 
         <div className='Present'>
