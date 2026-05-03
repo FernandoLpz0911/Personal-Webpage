caf68686ca18e5447057c7867ac650534e2d1473
flope8 <flope8@uic.edu>
2026-05-02 21:35:38 -0500

    fixed ordering

--- PersonalWebpage/src/components/About.jsx
+++ PersonalWebpage/src/components/About.jsx
@@ -20,17 +20,15 @@ const About = () => {
         <h2 className="section-title">
           About Me
         </h2>
-
-        <div className="education-block">
-          <h3 className="skills-title">Education & Coursework</h3>
-          <div className="uni-info">
-            <span className="uni-name">University of Illinois Chicago</span>
-            <span className="uni-degree">Bachelors in Computer Science ('27)</span>
-          </div>
-        </div>
-        
         <div className="about-content-wrapper">
           <div className="about-text">
+            <div className="education-block">
+              <h3 className="skills-title">Education & Coursework</h3>
+              <div className="uni-info">
+                <span className="uni-name">University of Illinois Chicago</span>
+                <span className="uni-degree">Bachelors in Computer Science ('27)</span>
+              </div>
+            </div>
             <p>
               I am a Senior Undergraduate Research Assistant as UIC with a large breadth in skills including basic frontend, backend,
               continuous integration (automated work), and developing machine learning models.
@@ -39,6 +37,7 @@ const About = () => {
               I am currently in the process of developing my research data and paper for submission at an IEEE conference while wrapping up final exams
               for Spring 2026. Continuing my work in the laboratory concurrently with my own independent research.
             </p>
+
             
             <div className="skills-container">
                <h3 className="skills-title">Technical Skills</h3>
